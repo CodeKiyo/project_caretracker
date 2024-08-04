4187fb135205a3eeb8f974e83cda9eb47bcf5037
francoooG <136312811+francoooG@users.noreply.github.com>
2024-08-04 12:39:39 +0800

    Notification View sorted Newest to oldest

--- app/src/main/java/com/mobdeve/caretracker/NotificationsPageFragment.kt
+++ app/src/main/java/com/mobdeve/caretracker/NotificationsPageFragment.kt
@@ -2,16 +2,23 @@ package com.mobdeve.caretracker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.mobdeve.caretracker.TestResultActivity.Companion.patientID
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 
 class NotificationsPageFragment : Fragment(R.layout.homemenu_notification_list_fragment) {
@@ -54,6 +61,7 @@ class NotificationsPageFragment : Fragment(R.layout.homemenu_notification_list_f
         return view
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun updateView() {
         // Update verticalRecyclerView
         val db = Firebase.firestore
@@ -61,6 +69,7 @@ class NotificationsPageFragment : Fragment(R.layout.homemenu_notification_list_f
         val userNotifications = userCollection.document().collection("Notifications")
         val data = ArrayList<NotificationModel>()
         val userid = arguments?.getString(USER_ID).toString()
+        val date = ArrayList<String>()
 
         userCollection
             .whereEqualTo("id", userid)
@@ -70,15 +79,40 @@ class NotificationsPageFragment : Fragment(R.layout.homemenu_notification_list_f
                     userCollection.document(document.id).collection("Notification")
                         .get()
                         .addOnSuccessListener { result ->
-                            for(document in result!!.documents) {
-                                println(document.id)
-                                val newData = NotificationModel(
-                                    document.get("date").toString(),
-                                    document.get("name").toString(),
-                                    document.get("oper").toString(),
-                                    document.get("type").toString()
-                                )
-                                data.add(newData)
+                            if (result != null && !result.isEmpty) {
+                                data.clear()
+                                for (document in result!!.documents) {
+                                    date.add(document.get("date").toString())
+                                }
+                            }
+
+                            val dateFormat = SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+                            val dateObjects = date.mapNotNull { dateString ->
+                                try {
+                                    dateFormat.parse(dateString)
+                                } catch (e: ParseException) {
+                                    null // Handle the exception or return a default value
+                                }
+                            }
+                            val sortedDateObjects = dateObjects.sortedDescending()
+                            val sortedDateStrings = sortedDateObjects.map { dateObject ->
+                                dateFormat.format(dateObject)
+                            }
+
+
+                            for(element in sortedDateStrings) {
+                                println(element)
+                                for(document in result!!.documents) {
+                                    if (document.get("date").toString() == element) {
+                                        val newData = NotificationModel(
+                                            document.get("date").toString(),
+                                            document.get("name").toString(),
+                                            document.get("oper").toString(),
+                                            document.get("type").toString()
+                                        )
+                                        data.add(newData)
+                                    }
+                                }
                             }
                             notificationsPageRecycler.adapter = NotificationAdapter(data, "Franco Carino")
                             notificationsPageRecycler.adapter?.notifyDataSetChanged()
@@ -91,5 +125,4 @@ class NotificationsPageFragment : Fragment(R.layout.homemenu_notification_list_f
                 println("Error getting documents: $exception")
             }
     }
-
 }
